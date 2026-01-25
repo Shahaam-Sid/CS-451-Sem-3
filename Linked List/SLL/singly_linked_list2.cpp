aa1437e9e62515f643f6f699cac5682f24301bfc
Shahaam-Sid <muhammadgc821@gmail.com>
2026-01-25 15:09:30 +0500

    Added More Methods to SLL

--- Linked List/SLL/singly_linked_list2.cpp
+++ Linked List/SLL/singly_linked_list2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-template <typename T>
-// OO Method
 
+// OO Method
+template <typename T>
 class SinglyLinkedList
 {
     private:
@@ -36,6 +36,46 @@ class SinglyLinkedList
                 curr->next = temp;
             }
             size++;
+        }
+        void insertSorted(T data)
+        {
+            Node* temp = new Node();
+            temp->element = data;
+            if (size == 0)
+            {
+                head = temp;
+                head->next = nullptr;
+            }
+            else if (head->element >= data)
+            {   
+                temp->next = head;
+                head = temp;
+            }
+            else
+            {
+                Node* curr = head;
+                Node* frwd = head->next;
+                while (frwd != nullptr && frwd->element < data)
+                {
+                    curr = curr->next;
+                    frwd = frwd->next;
+                }
+                curr->next = temp;
+                temp->next = frwd;
+
+            }
+            size++;
+        }
+        void insertUnique(T data)
+        {
+            int index = search(data);
+            if (index != -1)
+            {
+                std::cout << "Value exists at index " << index << std::endl;
+                return;
+            }
+
+            insert(data);
         }
         void display()
         {
@@ -73,6 +113,39 @@ class SinglyLinkedList
             }
             return -1;
         }
+        int* searchMultiple(T data)
+        {
+            if (size == 0) {return nullptr;}
+
+            int* indexes = new int[size];
+            Node* curr = head;
+
+            int index = 0;
+            while (curr->next != nullptr)
+            {
+                if (curr->element == data)
+                {
+                    indexes[index] = index;
+                }
+                else
+                {
+                    indexes[index] = -1;
+                }
+                curr = curr->next;
+                index++;
+
+            }
+            if (curr->element == data)
+            {
+                indexes[index] = index;
+            }
+            else
+            {
+                indexes[index] = -1;
+            }
+            
+            return indexes;
+        }
         void del(T data)
         {
             if (size == 0) {return;}
@@ -109,5 +182,89 @@ class SinglyLinkedList
                 current = current->next;
                 delete temp;
             }   
-        } 
+        }
 };
+
+
+
+
+
+int main()
+{
+
+    SinglyLinkedList<int> sll;
+
+    int choice;
+    int userInput;
+    int t;
+    std::cout << "Choose any one of the following: " << std::endl;
+    std::cout << "1 to Insert\n" << "2 to Insert Unique\n" <<
+                 "3 to Insert with Sort\n" << "4 to Display\n" <<
+                 "5 to Search\n" << "6 to Search Multiple Occurences\n" <<
+                 "7 to Delete\n" << "8 to Exit\n";
+
+    while (true)
+    {    
+        std::cout << "Enter: ";
+
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Input integer to insert: ";
+            std::cin >> userInput;
+            sll.insert(userInput);
+            break;
+        case 2:
+            std::cout << "Input integer to insert: ";
+            std::cin >> userInput;
+            sll.insertUnique(userInput);
+            break;
+        case 3:
+            std::cout << "Input integer to insert: ";
+            std::cin >> userInput;
+            sll.insertSorted(userInput);
+            break;
+        case 4:
+            std::cout << "Size: " << sll.getSize() << std::endl;
+            sll.display();
+            break;
+        case 5:
+            std::cout << "Input integer to search: ";
+            std::cin >> userInput;
+            t = sll.search(userInput);
+            if (t != -1){
+                std::cout << "Found at index: " << t << std::endl;
+            }
+            else {std::cout << "Element Not Found" << std::endl;}
+            break;
+        case 6:
+        {
+            std::cout << "Input integer to search: ";
+            std::cin >> userInput;
+            int* indexes = sll.searchMultiple(userInput);
+            std::cout << "Indexes: ";
+            for (int i = 0; i < sll.getSize(); i++)
+            {
+                if (indexes[i] != -1) {std::cout << indexes[i] << " ";}
+            }
+            std::cout << std::endl;
+            delete[] indexes;
+            
+            break;
+        }
+        case 7:
+            std::cout << "Input integer to delete: ";
+            std::cin >> userInput;
+            sll.del(userInput);
+            break;
+        case 8:
+            return 0;
+        default:
+            std::cout << "Invalid Input" << std::endl;
+            break;
+        }
+    }
+    return 0;
+}
